6abe5976ea0160d6bf334dcaed57a6aeb628bd7b
Austin-Spec <68626086+Austin-Spec@users.noreply.github.com>
2020-08-12 13:08:39 -0500

    Update paper.js

--- paper.js
+++ paper.js
@@ -16,8 +16,6 @@
         var angle = this.body.angle;
         push()
         translate(pos.x, pos.y)
-        rotate(angle)
-        rectMode(CENTER);
         strokeWeight(4)
         stroke("green")
         fill(255);
